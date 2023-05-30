26b4c84677eb27d626ee9c5a47bbb31fe19199bb
JaeHwaneee <rkdldk810@gmail.com>
2023-05-30 20:19:08 +0900

    윤년을 알아맞추기 테스트(JUnit)

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,13 @@
-// 윤년 확인하는 코드.
-
 public class Main {
     public static void main(String[] args) {
-        checkLeapYear();
-    }
-    // 1700, 2600 등 다양한 숫자 넣어보기
-    private static void checkLeapYear() {
-        int year = 2020;
-        // 4로 나누고, 100으로 정확히 나뉘어지며, 400으로 나뉘어 지는 숫자는 O표시 아닌 숫자는 X표시
-        if( (year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0) )
-            System.out.println(year + " O");
+        int i = 1700;
+        if ( checkLeapYear(i) )
+            System.out.println(i + " O");
         else
-            System.out.println((year + " X"));
+            System.out.println(i + " X");
+    }
+
+    public static boolean checkLeapYear(int year) {
+        return (year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0) ;
     }
 }
