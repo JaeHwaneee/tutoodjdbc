ad6506cfa09372e9c66b9cd3539f9bac9efb4280
JaeHwaneee <rkdldk810@gmail.com>
2023-05-30 19:46:33 +0900

    윤년 알아뭊추기(연산자)

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,10 @@ public class Main {
     public static void main(String[] args) {
         checkLeapYear();
     }
-
+    // 
     private static void checkLeapYear() {
         int year = 2020;
+        // 4로 나누고, 100으로 정확히 나뉘어지며, 400으로 나뉘어 지는 숫자는 O표시 아닌 숫자는 X표시
         if( (year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0) )
             System.out.println(year + " O");
         else
