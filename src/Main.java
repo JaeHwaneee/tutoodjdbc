5a6c7354b8d5ab197600f34ddf531289f2ebc6f1
JaeHwaneee <rkdldk810@gmail.com>
2023-05-30 19:31:11 +0900

    윤년 알아맞추기(연산자) - 1700, 2016, 2020, 1900, 2400, 2100, 2019 - 1700 X - 2020 0

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        checkLeapYear();
+    }
+
+    private static void checkLeapYear() {
+        int year = 2020;
+        if( (year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0) )
+            System.out.println(year + " O");
+        else
+            System.out.println((year + " X"));
+    }
+}
