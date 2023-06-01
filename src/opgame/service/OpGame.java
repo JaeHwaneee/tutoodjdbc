35e93ffc3cefc0cfac3656d33bb3a542fdc5adb2
JaeHwaneee <rkdldk810@gmail.com>
2023-06-01 09:45:44 +0900

    두 수를 더해서 9이하 문제출제, 여러 게임레벨 지원준비

--- src/opgame/service/OpGame.java
+++ src/opgame/service/OpGame.java
@@ -1,29 +1,30 @@
-package opgame.service;
+import opgame.service.PlusOperatorLevelOne;
 
 public class OpGame {
     int remainingAnswers;
     int a, b;
+    String cheeringUpMsg = "한번 더 해보자";
+
+    public OpGame(PlusOperatorLevelOne plusOperatorLevelOne) {
+
+    }
 
     public void makeQuestion() {
         remainingAnswers = 3;
-        a = 1;
         b = 1;
+    }
+
+    public void makeQuestion(int max) {
+        a = (int)(Math.random() * max);
+        b = (int)(Math.random() * (max - a));
     }
 
     public String getQuestion() {
         return String.format("%d + %d = ", a, b);
     }
-
-    public int getRemainingAnswers() {
-        return remainingAnswers;
-    }
-
-    public boolean isAnswer(int answer) {
-        remainingAnswers--;
-        return (a + b) == answer;
-    }
+}
 
     public String getCheeringUpMsg() {
-        return "한번 더 해보자";
+        return cheeringUpMsg;
     }
 }
